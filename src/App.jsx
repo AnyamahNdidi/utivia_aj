ca175ed6af9e3ebe70eba28f0987c826df1cf204
Anyamah Ndidi <anyamahedwin@gmail.com>
2023-04-07 14:56:47 +0100

    responsive header has been fix 80%

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from './pages/Home/Home';
 import School from './pages/Schools/School';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={ <Home/>} />
           <Route path="/school" element={ <School/>} />
